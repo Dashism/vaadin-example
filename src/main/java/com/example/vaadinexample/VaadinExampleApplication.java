55a813ec628542189e5590d7d1896afa3858bf33
dashism <benoitdarenne@hotmail.fr>
2023-11-23 16:02:02 +0100

    Delete one unused import

--- src/main/java/com/example/vaadinexample/VaadinExampleApplication.java
+++ src/main/java/com/example/vaadinexample/VaadinExampleApplication.java
@@ -17,7 +17,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
