d7e087d395e8bc69d5d63e1db5d956859420c346
dashism <benoitdarenne@hotmail.fr>
2023-11-23 15:51:11 +0100

    First commit

---
+++ src/test/java/com/example/vaadinexample/VaadinExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaadinexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
